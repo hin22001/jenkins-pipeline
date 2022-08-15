05aa7cb439d0c0a52ed0a8a8e851085b679f8f07
hin22001 <67498184+hin22001@users.noreply.github.com>
2022-08-15 15:16:28 +0800

    Update demo-pipeline.groovy

--- demo-pipeline.groovy
+++ demo-pipeline.groovy
@@ -4,7 +4,7 @@ def githubUrl = "https://github.com/hin22001/jenkins-pipeline.git"
 pipelineJob("Demo") {
   definition {
     cps {
-      script(readFileFromWorkspace('demo.groovy'))
+      script(readFileFromWorkspace('script/demo.groovy'))
       sandbox()
     }
   }
